2763777316697861260f212aab9897395bddb76c
samuel <samawyck@gmail.com>
2025-03-03 12:49:08 -0600

    Create app logic

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import dropDownManager from "./dropDownFactory.js";
+import "./styles.css";
+
+
+
+const app = (function() {
+    const header = document.querySelector("header");
+    const dropDownBtn = document.querySelector(".dropdown-1-btn");
+
+    const links = {
+        "Home": "#",
+        "Store": "#",
+        "Contact Us": "#"
+    };
+
+    const dropDown1 = dropDownManager.createDropDown(1, links);
+    header.appendChild(dropDown1.DocNode);
+
+    dropDownBtn.addEventListener("click", function() {
+        dropDown1.toggle();
+    });
+})();
